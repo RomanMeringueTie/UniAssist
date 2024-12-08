c17e129ae7071a09fc7a0b59d7afdef75f42f8bb
RomanMeringueTie <romankochetkoveducation@gmail.com>
2024-12-08 11:51:21 +0700

    Return enableEdgeToEdge and add safeDrawingPadding

--- app/src/main/java/ru/sibsutis/uniassist/MainActivity.kt
+++ app/src/main/java/ru/sibsutis/uniassist/MainActivity.kt
@@ -1,22 +1,20 @@
 package ru.sibsutis.uniassist
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import ru.sibsutis.core.network.KtorClient
 import ru.sibsutis.core.utils.daggerViewModel
-import ru.sibsutis.student.data.repository.StudentRepository
-import ru.sibsutis.student.data.service.StudentService
 import ru.sibsutis.student.di.DaggerStudentComponent
-import ru.sibsutis.student.domain.GetStudentScheduleUseCase
 import ru.sibsutis.student.presentation.StudentScheduleViewModel
 import ru.sibsutis.student.ui.StudentScheduleScreen
 import ru.sibsutis.uniassist.di.DaggerAppComponent
@@ -27,17 +25,23 @@ import ru.sibsutis.uniassist.navigation.SCHEDULE_ROUTE
 import ru.sibsutis.uniassist.ui.theme.UniAssistTheme
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         DaggerAppComponent.builder().build().inject(this)
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
             UniAssistTheme {
                 Scaffold(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .safeDrawingPadding(),
                     bottomBar = { BottomBar(navController = navController) }) {
-                    NavHost(navController = navController, startDestination = SCHEDULE_ROUTE) {
+                    NavHost(
+                        navController = navController,
+                        startDestination = SCHEDULE_ROUTE,
+                        modifier = Modifier.padding(it)
+                    ) {
                         composable(SCHEDULE_ROUTE) {
                             val component = DaggerStudentComponent.builder().build()
                             val viewModel: StudentScheduleViewModel =
