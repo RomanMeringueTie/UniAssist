283e8cefc33c8e153a8f918d94e6eb43c405514b
RomanMeringueTie <romankochetkoveducation@gmail.com>
2025-03-23 20:11:40 +0700

    More conflict resolving

---
+++ student/src/main/java/ru/sibsutis/student/presentation/StudentClassViewModel.kt
@@ -0,0 +1,47 @@
+package ru.sibsutis.student.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import ru.sibsutis.student.domain.GetStudentClassUseCase
+import ru.sibsutis.student.ui.ClassConverter
+import ru.sibsutis.student.ui.ClassUI
+
+class StudentClassViewModel(
+    private val classConverter: ClassConverter,
+    private val getStudentClassUseCase: GetStudentClassUseCase,
+    private val id: Int
+) :
+    ViewModel() {
+
+    private val _state: MutableStateFlow<State<ClassUI>> =
+        MutableStateFlow(State.Loading)
+    val state: StateFlow<State<ClassUI>> = _state
+
+
+    init {
+        loadClass(id)
+    }
+
+
+    private fun loadClass(id: Int) {
+        if (_state.value != State.Loading)
+            return
+        viewModelScope.launch {
+            val result = getStudentClassUseCase(id)
+            result.fold(
+                onSuccess = {
+                    _state.value =
+                        State.Content(content = classConverter.convertItem(it))
+                },
+                onFailure = {
+                    _state.value =
+                        State.Failure(message = it.message ?: "Unknown Error")
+                }
+            )
+        }
+    }
+
+}
